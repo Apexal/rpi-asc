e329f8d61b8b6e0279c3531a80efca525001f30f
Frank Matranga <thefrankmatranga@gmail.com>
2020-03-25 14:47:57 -0400

    Fixed Firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
 const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
+
+const db = admin.firestore()
 
 /**
  * On user creation, add data for the user in the 'users' collection.
