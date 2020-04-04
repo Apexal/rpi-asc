8c28d73c6f3df95a01853f88fe27bdb5958a9389
Frank Matranga <thefrankmatranga@gmail.com>
2020-04-04 01:13:32 -0400

    Proper time

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    queueEnd: dayjs('2020-04-04T18:11:00.000Z'),
+    queueEnd: dayjs('2020-04-04T18:30:00.000Z'),
     loaded: false,
     alerts: [],
     user: {
