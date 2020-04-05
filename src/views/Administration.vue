4847455ec9289a7494d4447491d863ee0f7df5f7
Frank Matranga <thefrankmatranga@gmail.com>
2020-04-05 19:53:02 -0400

    Reports on Administration

--- src/views/Administration.vue
+++ src/views/Administration.vue
@@ -2,7 +2,14 @@
   <div class="administration">
     <h1 class="mb-3">Administration</h1>
 
-    <h2>Current Students</h2>
+    <h2>Current Students ({{ allCurrentStudents.length }})</h2>
+    <div class="card my-3">
+      <div class="card-body d-flex text-center stats">
+        <span>{{ totalConversations }} total conversations</span>
+        <span>{{ contactLater.length }} contact later</span>
+        <span>{{ neverQueued.length }} never queued</span>
+      </div>
+    </div>
     <form @submit.prevent="addCurrentStudent" class="form-inline mb-3">
       <div class="input-group mr-2">
         <input
@@ -37,7 +44,7 @@
         </tr>
       </tbody>
     </table>
-    <h2>Accepted Students</h2>
+    <h2>Accepted Students ({{ allAcceptedStudets.length }})</h2>
     <table class="table table-sm">
       <thead class="thead-dark">
         <tr>
@@ -61,11 +68,38 @@
       </tbody>
     </table>
 
+    <hr />
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <h3>Contact Later</h3>
+
+        <div v-for="s in contactLater" :key="s.id" class="card mb-3">
+          <div class="card-body">
+            <h5 class="card-title">{{ s.name || 'Unnamed Student' }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{ s.id }}</h6>
+            <p class="card-text">
+              <!-- <span>{{ s.contactLaterDate }}</span>
+              <span>{{ s.contactLaterTime }}</span>
+              <span>{{ s.contactLaterTimezone }}</span>-->
+              <strong>{{ contactLaterDisplay(s) }}</strong>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-6">
+        <h3>Never Queued</h3>
+        <ul>
+          <li v-for="s in neverQueued" :key="s.id">{{ s.name || 'Unnammed' }} ({{ s.id }})</li>
+        </ul>
+      </div>
+    </div>
+
     <StudentModal v-if="selectedStudent" :student="selectedStudent" />
   </div>
 </template>
 
 <script>
+import dayjs from 'dayjs'
 import { db } from '@/firebase'
 import { mapGetters } from 'vuex'
 
@@ -87,7 +121,16 @@ export default {
     this.$bind('allAcceptedStudets', db.collection('accepted'))
   },
   computed: {
-    ...mapGetters(['userEmail'])
+    ...mapGetters(['userEmail']),
+    totalConversations () {
+      return this.allAcceptedStudets.reduce((acc, s) => acc + s.previouslyClaimedBy.length, 0)
+    },
+    contactLater () {
+      return this.allAcceptedStudets.filter(s => s.wantToBeContactedLater)
+    },
+    neverQueued () {
+      return this.allAcceptedStudets.filter(s => s.previouslyClaimedBy.length === 0 && !s.inQueue)
+    }
   },
   methods: {
     async addCurrentStudent () {
@@ -106,10 +149,29 @@ export default {
 
       await db.collection('current').doc(student.id).delete()
       this.$store.commit('ADD_ALERT', { type: 'success', text: (student.id || student.name) + ' is NO LONGER able to login and talk to accepted students.' })
+    },
+    contactLaterDisplay (s) {
+      if (s.contactLaterDate && s.contactLaterTime && s.contactLaterTimezone) {
+        const timeZone = s.contactLaterTimezone
+        const negative = timeZone.startsWith('-')
+        const hours = Math.abs(parseFloat(timeZone))
+
+        const timeZoneStr = (negative ? '-' : '+') + String(Math.floor(hours)).padStart(2, '0') + ':' + '00'
+
+        const final = s.contactLaterDate + 'T' + s.contactLaterTime + ':00' + timeZoneStr
+        const date = dayjs(final)
+
+        return date.format('MMMM D h:mm a')
+      }
+
+      return 'No preferred time given.'
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.stats span {
+  flex: 1;
+}
 </style>
