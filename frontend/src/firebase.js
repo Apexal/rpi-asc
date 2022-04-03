778ea85ee68fdaad69dddae199c9f0117bec05b9
Frank Matranga <thefrankmatranga@gmail.com>
2022-04-03 15:03:44 -0400

    Add firebase functions

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,6 +4,7 @@ import store from '@/store'
 import { initializeApp } from 'firebase/app'
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
 import { getAuth, connectAuthEmulator } from 'firebase/auth'
+import { getFunctions, connectFunctionsEmulator } from 'firebase/functions'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyB_Ll6jVASgD4taEIXAqL5zzXPMbVRgPYE',
@@ -20,10 +21,12 @@ const firebaseApp = initializeApp(firebaseConfig)
 
 export const db = getFirestore(firebaseApp)
 export const auth = getAuth(firebaseApp)
+export const functions = getFunctions(firebaseApp)
 
 if (location.hostname === 'localhost') {
   connectAuthEmulator(auth, 'http://localhost:9099')
   connectFirestoreEmulator(db, 'localhost', 8080)
+  connectFunctionsEmulator(functions, 'localhost', 5001)
 }
 
 auth.onAuthStateChanged(function (user) {
