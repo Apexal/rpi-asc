e6403f6a1b46033fb78aad4113e340c0cdc1fbc9
Frank Matranga <thefrankmatranga@gmail.com>
2022-04-03 15:03:24 -0400

    Update login page

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -4,29 +4,31 @@
       <h1 class="display-4">Welcome, students!</h1>
       <p class="lead">You will be able to chat 1-on-1 with current RPI students on this site.</p>
     </div>
+    <code>{{ status }}</code>
     <form class="form-inline login-form border p-3" @submit.prevent="login">
-      <span id="emailHelp" class="form-text text-muted">
-        Current RPI students must use their
-        <strong>school email</strong>. Accepted students can use
-        <strong>any email</strong>.
-      </span>
-      <div class="form-group mx-sm-2 my-0">
-        <input
-          v-model.trim="email"
-          type="email"
-          class="form-control"
-          id="email"
-          name="email"
-          placeholder="Email address"
-          required
-        />
-      </div>
-      <button
-        type="submit"
-        class="btn mr-2"
-        :class="loading ? 'btn-warning' : 'btn-primary'"
-        :disabled="loading"
-      >{{ loading ? 'Logging you in...' : 'Login' }}</button>
+        <span id="emailHelp" class="form-text text-muted">
+          Current RPI students must use their
+          <strong>@rpi.edu email</strong>. Accepted students can use their
+          <strong>personal email</strong>.
+        </span>
+        <div class="form-group mx-sm-2 my-0">
+          <input
+            v-model.trim="email"
+            type="email"
+            class="form-control"
+            id="email"
+            name="email"
+            placeholder="Email address"
+            :disabled="status === 1"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          class="btn mr-2"
+          :class="status === 1 ? 'btn-warning' : 'btn-primary'"
+          :disabled="status === 1"
+        >{{ status === 1 ? 'Sending sign-in email...' : 'Login' }}</button>
     </form>
   </div>
 </template>
@@ -36,25 +38,30 @@ import { auth, db } from '@/firebase'
 import { sendSignInLinkToEmail } from 'firebase/auth'
 import { collection, doc, getDoc } from 'firebase/firestore'
 
+const NOT_STARTED = 0
+const SENDING_EMAIL = 1
+const SENT_EMAIL = 2
+
 export default {
   name: 'Login',
   data () {
     return {
-      loading: false,
-      email: ''
+      email: '',
+      status: NOT_STARTED
     }
   },
   methods: {
     async login () {
       if (!this.email) return
-      this.loading = true
+      this.status = SENDING_EMAIL
+
       if (this.email.endsWith('@rpi.edu')) {
         // Check if this current student is on the allowed list
         // Allowed students will already have a document in the 'current' collection
         const userDoc = await getDoc(doc(collection(db, 'current'), this.email))
 
         if (!userDoc.exists()) {
-          this.loading = false
+          this.status = NOT_STARTED
           return alert('You have not been granted permission by the faculty.')
         }
       }
@@ -66,13 +73,13 @@ export default {
         }
         await sendSignInLinkToEmail(auth, this.email, options)
         localStorage.setItem('emailForSignIn', this.email)
+        this.status = SENT_EMAIL
         alert('Success! Check ' + this.email + ' for the sign-in link!')
       } catch (e) {
         localStorage.removeItem('emailForSignIn')
         alert(e)
+        this.status = NOT_STARTED
       }
-
-      this.loading = false
     }
   }
 }
